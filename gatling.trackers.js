9cecd97cbedbe48cbbf638cec80db2b02da9ee12
Your Name <garside@gmail.com>
2010-10-11 15:39:14 -0400

    Adding the trackers.js stub.

---
+++ gatling.trackers.js
@@ -0,0 +1,27 @@
+/**
+ *  Gatling Trackers
+ *
+ *  Copyright (c) 2010 My Sweet Website
+ */
+
+(function ($) {
+
+//------------------------------
+//  Declare Trackers
+//------------------------------
+
+$.gatling.declare(
+[
+	/* Add declarations here */
+]);
+
+//------------------------------
+//  Load Tracker Instances
+//------------------------------
+
+$.gatling.load(
+[
+	/* Add configurations here */
+]);
+        
+}(jQuery));
